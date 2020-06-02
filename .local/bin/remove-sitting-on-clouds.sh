d0a1f6b3fba5f7e4444362fd2d739a1f7b693d47
David Bernheisel <david@bernheisel.com>
2020-06-02 11:14:34 -0400

    scripts: add music cleaning script

--- .local/bin/remove-sitting-on-clouds.sh
+++ .local/bin/remove-sitting-on-clouds.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 display_usage() {
   echo "This will scan all MP3s and FLACs and remove comments and URLs"
@@ -10,25 +10,25 @@ display_usage() {
 [[ $1 == "" ]] && display_usage && exit 1
 [[ $1 == "--help" ]] && display_usage && exit 0
 
-for f in "$1"/*/*.mp3; do
-  if type kid3-cli &>/dev/null; then
-    echo "Fixing $f"
-    kid3-cli \
-      -c 'set "User-defined URL" ""' \
-      -c 'set "Comment" ""' \
-      "$f"
-  else
+for f in "$1"/**/{.,}*.mp3(N); do
+  if ! type kid3-cli &>/dev/null; then
     echo "This requires kid3-cli to be installed to clean MP3s"
     exit 1
   fi
+
+  echo "Fixing $f"
+  kid3-cli \
+    -c 'set "User-defined URL" ""' \
+    -c 'set "Comment" ""' \
+    "$f"
 done
 
-for f in "$1"/*/*.flac; do
-  if type metaflac &>/dev/null; then
-    echo "Fixing $f"
-    metaflac --remove-tag=WWW --remove-tag=COMMENT "$f"
-  else
+for f in "$1"/**/{.,}*.flac(N); do
+  if ! type metaflac &>/dev/null; then
     echo "This requires metaflac (installed w/ flac) to be installed to clean FLACs"
     exit 1
   fi
+
+  echo "Fixing $f"
+  metaflac --remove-tag=WWW --remove-tag=COMMENT "$f"
 done
