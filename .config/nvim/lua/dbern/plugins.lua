c958b067d7dcbd9ec43b2a4439de4cb722c05992
David Bernheisel <david@bernheisel.com>
2024-12-27 00:06:50 -0500

    neovim: fixup multicursor

--- .config/nvim/lua/dbern/plugins.lua
+++ .config/nvim/lua/dbern/plugins.lua
@@ -98,6 +98,11 @@ require("lazy").setup({
   },
   { 'mfussenegger/nvim-jdtls' },
 
+  {
+      "3rd/image.nvim",
+      opts = {}
+  },
+
   -- Snippets
 
   -- Completion
@@ -339,26 +344,77 @@ require("lazy").setup({
   { 'itchyny/calendar.vim',
     config = function() require('dbern.plugins.calendar') end },
 
-  -- <C-n> to select next word with new cursor
-  { 'brenton-leighton/multiple-cursors.nvim',
-    version = '*',
-    opts = {
-      pre_hook = function()
-        require("cmp").setup({ enabled = false })
-      end,
-      post_hook = function()
-        require("cmp").setup({ enabled = true })
-      end
-    },
-    keys = {
-      -- {"<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = {"n", "x"}, desc = "Add cursor and move down"},
-      -- {"<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = {"n", "x"}, desc = "Add cursor and move up"},
-      {"<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = {"n", "i"}, desc = "Add or remove cursor"},
-      {"<c-n>", "<Cmd>MultipleCursorsAddMatches<CR>", mode = {"n", "x"}, desc = "Add cursors to cword"},
-      -- {"<Leader>d", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = {"n", "x"}, desc = "Add cursor and jump to next cword"},
-      -- {"<Leader>D", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = {"n", "x"}, desc = "Jump to next cword"},
-      -- {"<leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = {"n", "x"}, desc = "Lock virtual cursors"},
-    }
+  { "jake-stewart/multicursor.nvim",
+    branch = "1.0",
+    config = function()
+        local mc = require("multicursor-nvim")
+        mc.setup()
+        local set = vim.keymap.set
+        -- Add or skip adding a new cursor by matching word/selection
+        set({"n", "v"}, "<c-n>",
+            function() mc.matchAddCursor(1) end)
+        set({"n", "v"}, "<c-m>",
+            function() mc.matchSkipCursor(1) end)
+        set({"n", "v"}, "<c-s-n>",
+            function() mc.matchAddCursor(-1) end)
+        set({"n", "v"}, "<c-s-m>",
+            function() mc.matchSkipCursor(-1) end)
+
+        -- Rotate the main cursor.
+        set({"n", "v"}, "<left>", mc.nextCursor)
+        set({"n", "v"}, "<right>", mc.prevCursor)
+
+        -- Delete the main cursor.
+        set({"n", "v"}, "<c-x>", mc.deleteCursor)
+
+        -- Add and remove cursors with control + left click.
+        set("n", "<c-leftmouse>", mc.handleMouse)
+
+        -- Easy way to add and remove cursors using the main cursor.
+        set({"n", "v"}, "<c-q>", mc.toggleCursor)
+
+        set("n", "<esc>", function()
+            if not mc.cursorsEnabled() then
+                mc.enableCursors()
+            elseif mc.hasCursors() then
+                mc.clearCursors()
+            else
+                -- Default <esc> handler.
+            end
+        end)
+
+        -- Align cursor columns.
+        set("n", "<leader>a", mc.alignCursors)
+
+        -- Split visual selections by regex.
+        set("v", "S", mc.splitCursors)
+
+        -- Append/insert for each line of visual selections.
+        set("v", "I", mc.insertVisual)
+        set("v", "A", mc.appendVisual)
+
+        -- match new cursors within visual selections by regex.
+        set("v", "M", mc.matchCursors)
+
+        -- Rotate visual selection contents.
+        -- set("v", "<leader>t",
+        --     function() mc.transposeCursors(1) end)
+        -- set("v", "<leader>T",
+            -- function() mc.transposeCursors(-1) end)
+
+        -- Jumplist support
+        set({"v", "n"}, "<c-i>", mc.jumpForward)
+        set({"v", "n"}, "<c-o>", mc.jumpBackward)
+
+        -- Customize how cursors look.
+        local hl = vim.api.nvim_set_hl
+        hl(0, "MultiCursorCursor", { link = "Cursor" })
+        hl(0, "MultiCursorVisual", { link = "Visual" })
+        hl(0, "MultiCursorSign", { link = "SignColumn"})
+        hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
+        hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+        hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
+    end
   },
 
   -- Easier block commenting.
@@ -480,4 +536,8 @@ require("lazy").setup({
   { 'junegunn/limelight.vim' },
   { 'junegunn/goyo.vim' },
   { 'reedes/vim-wordy' },
-}, {})
+}, {
+  rocks = {
+    hererocks = true,
+  },
+})
